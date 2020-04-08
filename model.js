797c8dfaa51cb9606f19834f7c8a209a7c947c06
Arsene Ferriere <arsene.ferriere@mines-paristech.fr>
2020-04-08 20:19:49 +0200

    Fini sauf kill

--- model.js
+++ model.js
@@ -1,37 +1,42 @@
 const svgNS = "http://www.w3.org/2000/svg";
 
 //constantes graphiques
-const width = 600;
-const height = 600;
+const width = 600;          //initalisation largeur
+const height = 600;         //Initialisation hauteur
 
 
 //Constantes de temps
 const v = 20;   //20px par secondes
 const T = 1/25;   // 1/nb de frame par secondes
 const dl = v*T;   // deplacement elementaire
-let t = 0;
+let t = 0;      //Durée depuis le début de l'experience
 let needtoplot = true;
 
 //Constante liées a la maladie
-let T_mort = 400/T; //Durée pour mourir
-let T_guerri = 50/T; //Durée pour guerrir
+let T_mort = 400/T; //Durée pour mourir en tics car divise par la periode (400 secondes en théorie)
+let T_guerri = 50/T; //Durée pour guerrir 50 secondes en theorie
 let Transm = 0.5; //probabilité de transmettre
 
-let nb_sain = 249;
-let nb_malade = 0;
+let nb_sain = 249;  //nombre de personnes saines
+let nb_malade = 0; //nombre de personnes maldes
 let nb_guerri = 0;
-let Contamines = new Array;
-let Sains = new Array;
-let Guerris = new Array;
-let Particules = new Array;
+let N = nb_sain + nb_malade + nb_guerri;
+let Contamines = new Array; //nombre de contaminees AU COURS DU TEMPS
+let Sains = new Array;      //nombre de sains AU COURS DU TEMPS
+let Guerris = new Array;    //nombre de guerrisAU COURS DU TEMPS
+
+let Particules = new Array; //initialisation du tableau avec toutes les particules
 
 
 
 
 
-/* generates random circles in specified area */
 class particule {
-    
+    /** 
+    * Classe définissant une particule
+    * Elle est définit par sa vitesse, sa position, sa taille et son état.
+    * On lui rajoute un attribue time decrivant la durée depuis laquelle elle est infecté
+    */
     constructor(start_x, start_y, radius,dx,dy,state) {
         this.x = start_x;
         this.y = start_y;
@@ -44,12 +49,16 @@ class particule {
     }
     
     draw() {
+        /**
+         * Fonction que l'on appelle a l'apparition d'un particule
+         * Permet de créer le cercle qui la représente.
+         * La couleur dépend de son etat
+         */
         let svg = document.querySelector("svg");
         let c = document.createElementNS(svgNS, 'circle');
         c.setAttribute('cx', this.x); // svg's circle center
         c.setAttribute('cy', this.y);
         c.setAttribute('r', this.r);  // svg's circle radius
-        c.setAttribute('class','part')
         switch (this.state) {
             case 0:
                 c.setAttribute('fill', 'yellow')
@@ -64,10 +73,14 @@ class particule {
                 c.setAttribute('fill','black')
                 break     
         }
-        svg.append(c)
+        svg.append(c) //on ajoute notre creation au container svg
     }
 
     mur(){
+        /**
+         * Detecte la colision a un mur
+         * Le choc est elastique
+         */
         if (this.x-this.r < 0){
             this.dx = Math.abs(this.dx)
         }else{if (this.x + this.r>width){
@@ -82,23 +95,29 @@ class particule {
     }
 
     transmition(autre){
+        /**
+         * Transmet l'etat 1 avec une probabilité Transm
+         */
         if (this.state == 1){
             r = Math.random()
             if (autre.state == 0 & r < Transm){
-                autre.state = 1
-                autre.time = 0;
-                nb_malade ++;
-                nb_sain --;
+                autre.state = 1; //etat transmit
+                autre.time = 0;  
+                nb_malade ++; //un nouveau malade
+                nb_sain --; //un sain de moins
                 needtoplot = true;
             }
         }
     }
 
     colision(autre){
-        let dist = Math.pow(autre.x-this.x,2) + Math.pow(autre.y-this.y,2);
-        if (dist <= Math.pow(autre.r+this.r+2,2)){
-            autre.dx = 2*dl*(Math.random()-0.5);
-            autre.dy = 2*dl*(Math.random()-0.5);
+        /**
+         * Est appele lorsque deux particules rentrent en contact
+         */ 
+        let dist = Math.pow(autre.x-this.x,2) + Math.pow(autre.y-this.y,2); //(Dx**2+Dy**2)
+        if (dist <= Math.pow(autre.r+this.r+2,2)){ //plus petit que la somme des rayons
+            autre.dx = 2*dl*(Math.random()-0.5);  //Nouvelle direction de déplacement aleatoire
+            autre.dy = 2*dl*(Math.random()-0.5);  
             
             this.dx = 2*dl*(Math.random()-0.5);
             this.dy = 2*dl*(Math.random()-0.5);
@@ -108,10 +127,13 @@ class particule {
         }
     }
     ChangementEtat(){
+        /**
+         * Est on malade depuis suffisament longtemps pur être guerri ?
+         */
         if (this.time > T_guerri){
             if (this.state == 1){
                 this.state = 2;
-                this.time = NaN;
+                this.time = NaN; //plus besoin de compter le temps
                 nb_guerri ++;
                 nb_malade --;
                 needtoplot = true;
@@ -120,9 +142,14 @@ class particule {
     }
     
     deplacement(){
-        this.ChangementEtat();
-        this.mur();
-        this.x = this.x + this.dx;
+        /**
+         * Evolution de la particule entre la frame actuelle
+         * et la suivante
+         * L'impact entre les particules sera calculer en amont
+         */
+        this.ChangementEtat(); //on change d'etat ?
+        this.mur(); //on cogne un mur ?
+        this.x = this.x + this.dx; //deplacement 
         this.y = this.y + this.dy;
         this.time += 1;
         }
@@ -130,14 +157,14 @@ class particule {
     
 
 function sliding(){
+    /**
+     * Donne une valeur au variable en fonction des curseurs
+     */
     var slider_guerri = document.getElementById("T_guerri");
     var slider_transm = document.getElementById("Transm");
     var slider_pop = document.getElementById("Pop");
-    T_guerri = slider_guerri.value/T;
-    Transm = slider_transm.value/100;
-    nb_sain = slider_pop.value-1;
 
-
+    //si on deplace le slider
     slider_guerri.oninput = function() {
         T_guerri = this.value/T;
     }
@@ -145,44 +172,91 @@ function sliding(){
         Transm = this.value/100;
     }
     slider_pop.oninput = function() {
-        if (this.value-1 < nb_sain){
-            Particules = Particules.slice(nb_sain-this.value+1)
-            console.log('en dessous')
-            for (var i =0;i<= nb_sain-this.value+1; i++){
-                svg.removeChild(svg.firstChild);
+        var x; 
+        var y;
+        var r;
+        var dx;
+        var dy;
+        var s;
+        var part;
+        /*  On ne peut pas simplement changer nb_sain
+            Car il faut aussi changer le t'nregistrment 
+            des particules.
+        
+        */
+        N = nb_malade + nb_guerri + nb_sain;
+        let newN = this.value;
+        let svg = document.querySelector("svg");
+        console.log(this.value)
+        if (newN < N){
+            /* Si on est plus bas que l'ancienne valeur de N
+               Il faut enlever des cases
+            */
+            Particules = Particules.slice(0,newN) //on enlève la difference
+            for ( var i = 0; i < N - newN; i++){
+                svg.removeChild(svg.lastChild);
             }
         }else{
-            for(let i=0;i<this.value-1-nb_sain;i++){
+            for(var i=0; i < newN - N;i++){
                 x = Math.random()*width;
                 y = Math.random()*height;
                 r = Math.floor(Math.random()*3+3);
                 dx = 2*dl*(Math.random()-0.5);
                 dy = 2*dl*(Math.random()-0.5);
-                let part = new particule(x, y, r, dx, dy, 0)
-                Particules.unshift(part);
+                part = new particule(x, y, r, dx, dy, 0)
+                Particules.push(part);
                 part.draw()
             }
         }
-        nb_sain = this.value-1;
+    s = 0;
+    i = 0;
+    r =0;
+    for(var part of Particules){
+        switch (part.state){
+            case 0:
+                s++
+                break;
+            case 1:
+                i++;
+                break;
+            case 2:
+                r++;
+                break;
+            case -1:
+                break  
+            
+        }
     }
-    let svg = document.querySelector("svg");
+    nb_sain = s;
+    nb_guerri = r;
+    nb_malade = i;
+    N = s + r + i;
+    console.log(N)
     Newframe(svg);
+    }
+
 }
     
 
 
 
-for (var i = 0; i < nb_sain; i++) {
-    x = Math.random()*width;
-    y = Math.random()*height;
-    r = Math.floor(Math.random()*3+3);
-    dx = 2*dl*(Math.random()-0.5);
-    dy = 2*dl*(Math.random()-0.5);
-    Particules.push(new particule(x, y, r, dx, dy, 0));
-  }
-add()
 
 function InitMonde(){
+    var slider_guerri = document.getElementById("T_guerri");
+    var slider_transm = document.getElementById("Transm");
+    var slider_pop = document.getElementById("Pop");
+    T_guerri = slider_guerri.value/T;
+    Transm = slider_transm.value/100;
+    nb_sain = slider_pop.value-1;
+    add()
+    for (var i = 0; i < nb_sain; i++) {
+        x = Math.random()*width;
+        y = Math.random()*height;
+        r = Math.floor(Math.random()*3+3);
+        dx = 2*dl*(Math.random()-0.5);
+        dy = 2*dl*(Math.random()-0.5);
+        Particules.push(new particule(x, y, r, dx, dy, 0));
+        }
     for(var i of Particules){
         i.draw()
     }
@@ -276,21 +350,20 @@ function Newframe(svg){
     for (var i = 0; i < Particules.length; i += 1){
         var part = Particules[i]
         part.deplacement();
-        let rond = Ronds[i]
-        rond.setAttribute('cx',part.x)
-        rond.setAttribute('cy',part.y)
+        Ronds[i].setAttribute('cx',part.x)
+        Ronds[i].setAttribute('cy',part.y)
         switch (part.state) {
             case 0:
-                rond.setAttribute('fill', 'yellow')
+                Ronds[i].setAttribute('fill', 'yellow')
                 break;
             case 1:
-                rond.setAttribute('fill', 'pink');
+                Ronds[i].setAttribute('fill', 'pink');
                 break;
             case 2:
-                rond.setAttribute('fill','blue');
+                Ronds[i].setAttribute('fill','blue');
                 break;
             case -1:
-                rond.setAttribute('fill','black')
+                Ronds[i].setAttribute('fill','black')
                 break     
         }
     }
@@ -301,14 +374,15 @@ function Newframe(svg){
         Guerris.push({x:t, y:nb_guerri/N});
         graph()
         needtoplot = false
-        console.log(Transm)
     }
 }
+
+
+
 
 window.onload = function () { 
     InitMonde()
     let svg = document.querySelector("svg");
-    console.log(svg)
     svg.setAttribute('width', width);
     svg.setAttribute('height', height);
     Newframe(svg);
